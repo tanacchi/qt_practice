556751347c7ccb68044f3c394f756dc438efc56b
tanacchi <q111026d@mail.kyutech.jp>
2019-06-17 17:36:52 +0900

    Add text field

--- memo/main.cpp
+++ memo/main.cpp
@@ -1,11 +1,12 @@
 #include "mainwindow.hpp"
 #include <QApplication>
+#include <QTextEdit>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QApplication app(argc, argv);
+    QTextEdit text_edit;
+    text_edit.show();
 
-    return a.exec();
+    return app.exec();
 }
