e6ee82a40e29c2a3ac9e8a924e7026725ea91c51
tanacchi <q111026d@mail.kyutech.jp>
2019-06-17 17:52:34 +0900

    Add quit button

--- memo/main.cpp
+++ memo/main.cpp
@@ -1,12 +1,24 @@
 #include "mainwindow.hpp"
 #include <QApplication>
 #include <QTextEdit>
+#include <QtWidgets>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QTextEdit text_edit;
-    text_edit.show();
+    QTextEdit* text_edit = new QTextEdit;
+    QPushButton* quit_button = new QPushButton{"&Qiut"};
+
+    QObject::connect(quit_button, SIGNAL(clicked()), qApp, SLOT(quit()));
+
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->addWidget(text_edit);
+    layout->addWidget(quit_button);
+
+    QWidget window;
+    window.setLayout(layout);
+
+    window.show();
 
     return app.exec();
 }
