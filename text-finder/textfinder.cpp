a3945cf7840a2eee5ad7a530453cc41f18c97fbc
tanacchi <q111026d@mail.kyutech.jp>
2019-06-19 01:40:03 +0900

    Link func "on_pushButton_clicked" to UI and Add file loader

--- text-finder/textfinder.cpp
+++ text-finder/textfinder.cpp
@@ -1,14 +1,38 @@
 #include "textfinder.hpp"
 #include "ui_textfinder.h"
 
+#include <QFile>
+#include <QTextStream>
+
 TextFinder::TextFinder(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::TextFinder)
 {
     ui->setupUi(this);
+    load_text_file();
 }
 
 TextFinder::~TextFinder()
 {
     delete ui;
+}
+
+void TextFinder::on_pushButton_clicked()
+{
+    QString search_str = ui->lineEdit->text();
+    ui->textEdit->find(search_str, QTextDocument::FindWholeWords);
+}
+
+void TextFinder::load_text_file()
+{
+    QFile input_file(":/input.txt");
+    input_file.open(QIODevice::ReadOnly);
+
+    QTextStream istream(&input_file);
+    QString input_str = istream.readAll();
+    input_file.close();
+
+    ui->textEdit->setPlainText(input_str);
+    QTextCursor cursor = ui->textEdit->textCursor();
+    cursor.movePosition(QTextCursor::Start, QTextCursor::MoveAnchor, 1);
 }
--- text-finder/textfinder.hpp
+++ text-finder/textfinder.hpp
@@ -15,8 +15,13 @@ public:
     explicit TextFinder(QWidget *parent = nullptr);
     ~TextFinder();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::TextFinder *ui;
+
+    void load_text_file();
 };
 
 #endif // TEXTFINDER_HPP
