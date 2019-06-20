6a50a30ddb5dcb4c679ed3ccfd76173ddeb7094a
tanacchi <q111026d@mail.kyutech.jp>
2019-06-20 16:06:50 +0900

    Add minimum html render

--- simple-browser/main.cpp
+++ simple-browser/main.cpp
@@ -1,10 +1,18 @@
 #include "simplebrowser.hpp"
 #include <QApplication>
+#include <QWebEngineView>
+#include <QUrl>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     SimpleBrowser w;
+    QWebEngineView* view = new QWebEngineView(&w);
+
+    view->load(QUrl("https://google.com"));
+    view->resize(400, 600);
+    view->show();
+
     w.show();
 
     return a.exec();
