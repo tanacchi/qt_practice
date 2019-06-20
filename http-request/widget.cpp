2cbb01f17a4544ea42f89dda7617e2be08a3228a
tanacchi <q111026d@mail.kyutech.jp>
2019-06-20 17:27:50 +0900

    Create project of http-request

---
+++ http-request/widget.cpp
@@ -0,0 +1,19 @@
+#include "widget.hpp"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::on_pushButton_clicked()
+{
+
+}
